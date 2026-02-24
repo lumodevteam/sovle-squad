4d868697c78d674e41558d6d27053d9358061f18
nvran2 <nvran2@ocdsb.ca>
2026-02-24 12:01:59 -0500

    Reorganizing numbers and number line

--- solve-squad/scenes/entities/numberLine/numberLine.gd
+++ solve-squad/scenes/entities/numberLine/numberLine.gd
@@ -1,21 +1,9 @@
 extends Sprite2D
-var isDragging = false # state management
-var mouseOffset #center's the mouse on click
-var delay = 10
+
 enum State { #different parts of line and numbers
 	middle,
 	right,
 	left,
-	numOne,
-	numTwo,
-	numThree,
-	numFour,
-	numFive,
-	numSix,
-	numSeven,
-	numEight,
-	numNine,
-	numZero,
 }
 
 func change_state(state) -> void:
@@ -27,18 +15,3 @@ func ready() -> void:
 	var clone = originSprite.duplicate()
 	add_child(clone)
 	clone.position = Vector2(32,0) # sets location of next part of number line number line
-
-
-func _physics_process(delta):
-	if isDragging == true:
-		var tween = get_tree().create_tween()
-		tween.tween_property(self, "position", get_global_mouse_position(), delay * delta)
-
-func _input(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			if get_rect().has_point(to_local(event.position)):
-				print('clicked')
-				isDragging = true
-			else:
-				isDragging = false
---
+++ solve-squad/scenes/entities/numberLine/numbers.gd
@@ -0,0 +1,35 @@
+extends Sprite2D
+var isDragging = false # state management
+var mouseOffset #center's the mouse on click
+var delay = 10
+enum State { #different parts of line and numbers
+	numOne,
+	numTwo,
+	numThree,
+	numFour,
+	numFive,
+	numSix,
+	numSeven,
+	numEight,
+	numNine,
+	numZero,
+}
+
+func change_state(state) -> void:
+	$sprite.set_frame(state)
+
+@onready var originSprite = $sprite
+
+func _physics_process(delta):
+	if isDragging == true:
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", get_global_mouse_position(), delay * delta)
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			if get_rect().has_point(to_local(event.position)):
+				print('clicked')
+				isDragging = true
+			else:
+				isDragging = false
