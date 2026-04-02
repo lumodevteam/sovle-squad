0b7423de6686fe6587d15c26cf735503097e997c
nvran2 <nvran2@ocdsb.ca>
2026-04-02 13:57:43 -0400

    finished number line (boo-yeah)

--- solve-squad/scenes/entities/numberLine/numbers.gd
+++ solve-squad/scenes/entities/numberLine/numbers.gd
@@ -12,9 +12,14 @@ var body_position: Vector2
 var snapping: bool = false
 var current_snap: Vector2 = Vector2.INF
 var tween: Tween  # store tween so we can kill it before making a new one
+var in_right_position: bool = false
 
 func _ready() -> void:
 	SnapManager.body_entered.connect(_on_body_entered)
+	SnapManager.all_correct.connect(_on_all_correct)
+	
+func _on_all_correct() -> void:
+	print("congrats!!")
 
 func _on_body_entered(body_position: Vector2, id: int) -> void:
 	if self.id == id:
@@ -28,7 +33,9 @@ func _on_body_entered(body_position: Vector2, id: int) -> void:
 			isDragging = false
 			self.body_position = body_position
 			snapping = true
-			print(SnapManager.num_in_right_place(body_position))
+			in_right_position = SnapManager.num_in_right_place(body_position)
+			if in_right_position:
+				SnapManager.correct += 1
 
 func change_state(state: int) -> void:
 	animated_sprite.frame = state
@@ -50,6 +57,9 @@ func _unhandled_input(event: InputEvent) -> void:
 				if current_snap != Vector2.INF:
 					SnapManager.release(current_snap, self)
 					current_snap = Vector2.INF
+					if in_right_position:
+						in_right_position = false
+						SnapManager.correct -= 1
 				get_viewport().set_input_as_handled()
 		else:
 			isDragging = false
